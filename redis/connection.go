f171d72f7a0d79563996ec5931614621e0a6d44d
Richard <richard@Richards-MacBook-Pro.local>
2019-08-27 16:37:57 +0700

    fix connect redis

--- redis/connection.go
+++ redis/connection.go
@@ -1,13 +1,12 @@
 package redis
 
 import (
-	"github.com/go-xtek/cse-pos/base/redis"
 	"github.com/go-xtek/vuvo-go/l"
 )
 
 // ConnectRedis ...
-func ConnectRedis(uri string) redis.Store {
-	redisStore := redis.NewWithPool(uri)
+func ConnectRedis(uri string) Store {
+	redisStore := NewWithPool(uri)
 	_, err := redisStore.GetString("_test_")
 	if err != nil {
 		ll.Fatal("Unable to connect to Redis", l.Error(err), l.String("ConnectionString", uri))
